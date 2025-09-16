191c04f00e79d2e46eedb49f14ae0ae7d4421f68
flipixcool <flipixcool>
2025-09-17 00:20:43 +0300

    Сделал первое задание по 2 лабе

--- src/arrays.py
+++ src/arrays.py
@@ -1,14 +1,10 @@
-﻿# ЛР2
-#### Задание 1
-
-```Python
 def min_max(arr):
     if len(arr) == 0:
         return 'ValueError'
     else:
         return (min(arr), max(arr))
 def unique_sorted(arr):
-    return tuple(set(sorted(arr)))
+    return list(set(sorted(arr)))
 def flatten(arr):
     lst = []
     for i in arr:
@@ -18,9 +14,5 @@ def flatten(arr):
             else:
                 return 'TypeError'
     return lst
-arr = [3, -1, 5, 5, 0]
-print(min_max(arr))
-```
-![alt text](images/image-1.1.png)
-![alt text](images/image-1.2.png)
-![alt text](images/image-1.3.png)
+arr = [[1, 2], [3, 4]] 
+print(flatten(arr))
